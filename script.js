5466ede48dec0a2636dec8d6de54712fec765708
Oriane <oriane.louis.pro@gmail.com>
2020-02-25 23:05:38 +0100

    Display next question

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ class Quizz {
     }
 
     async launch() {
+        var _this = this;
         var counter = 0;
         var form = document.getElementById("formAnswer");
+
         this.questions = await this.getJson()
                                     .then(data => data.questions)
                                     .catch(e => console.error(e));
@@ -15,21 +17,72 @@ class Quizz {
         this.displayNextQuestion(counter);
         form.addEventListener("submit", function(event) {
             event.preventDefault();
+
+            if (counter < _this.questions.length) {
+                
+                var userAnswer = _this.getUserAnswer();
+                var indexCorrectAnswer = _this.questions[counter].answer-1;
+                var correctAnswer = _this.questions[counter].propositions[indexCorrectAnswer];
+                var labels = document.querySelectorAll("label");
+                
+                labels[indexCorrectAnswer].style.color = "green";
+                if (userAnswer.checked.value !== correctAnswer) {
+                    labels[userAnswer.index].style.color = "red";
+                }
+
+                setTimeout(() => {
+                    _this.displayNextQuestion(++counter);
+                    _this.resetColor();
+                }, 2000);
+
+            }
         });
     }
 
     displayNextQuestion(index) {
         var container = document.getElementsByClassName("question")[0];
-        var btns = document.getElementsByTagName("button");
+        var labels = document.querySelectorAll("label");
+        var radioInput = document.querySelectorAll("input[type=radio]");
 
-        container.innerHTML = this.questions[index].question;
-
-        for (var i = 0; i < btns.length; i++) {
-            btns[i].innerHTML = this.questions[index].propositions[i];
+        if (this.questions[index]) {
+            container.innerHTML = this.questions[index].question;
+            for (var i = 0; i < labels.length; i++) {
+                var answer = this.questions[index].propositions[i];
+                radioInput[i].value = answer;
+                labels[i].setAttribute("for", answer);
+                labels[i].innerHTML = answer;
+            }
         }
     }
 
-    async getJson() {
+    resetColor() {
+        var labels = document.querySelectorAll("label");
+        for (var i =0 ; i < labels.length; i++) {
+            labels[i].style.color = "black";
+        }
+    }
+
+    getUserAnswer() {
+        var radios = document.querySelectorAll('input[type="radio"]:checked');
+        var value = (radios.length > 0) ? radios[0] : null;
+        return {checked: value, index: this.getIndex()};
+    }
+
+    getIndex() {
+        var input = document.getElementsByTagName('input');
+        var index;
+        for (var i = 0; i < input.length; i++) {
+            if (input[i].type="radio") {
+                if (input[i].checked) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        return index;
+    }
+
+    getJson() {
         var json = "./data.json";
         return fetch(json)
                 .then(response => response.json())
